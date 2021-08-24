6aa1970f5deb50696edad91c44761b3530acf53d
Zukhriddin <zuxr.akhmedov@gmail.com>
2021-08-24 17:34:07 +0500

    react app has been created to create a restaurant website

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
 
+import MyNavBar from './components/MyNavBar';
+import Home from './components/Home'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/*Prop is is a dynamic piece of info that is passed*/}
+      <MyNavBar title="Restaurant" color="light" />
+
+      <Home />
     </div>
   );
 }
